bed08497d935d7c206cd3a5091b0948f77cec603
Ankit <ankitchauhan21500@gmail.com>
2022-12-11 17:58:35 +0530

    replaced API, corrected localStorage errors, optimied the code

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function time() {
     var s = d.getSeconds();
     var m = d.getMinutes();
     var h = d.getHours();
+    if(h)
     span.textContent =
         ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
 }
